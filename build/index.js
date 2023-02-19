50f7f840d43829b11c3a560379c3df7e0f0e0878
Richard Goshen <rick.goshen@gmail.com>
2023-02-19 13:56:48 -0700

    automate build and compile

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('hi there');
+console.log('12345');
+const logSomething = () => {
+    console.log('something');
+};
+logSomething();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 console.log('hi there');
+console.log('12345');
+
+const logSomething = ()=>{
+	console.log('something');
+}
+
+logSomething();
