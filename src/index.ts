f687855fef4a224bab0c796a2955b88dff70291a
Richard Goshen <rick.goshen@gmail.com>
2023-02-19 14:21:18 -0700

    setup sorter scaffold

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 class Sorter {
-  collection: number[];
+  constructor(public collection: number[]) {}
+
+  sort(): void {}
 }
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
