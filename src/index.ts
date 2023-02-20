4b6f94f992db6eb5f1cc61fc21cd7b46ceb20170
Richard Goshen <rick.goshen@gmail.com>
2023-02-19 20:03:50 -0700

    add sorting for linked lists

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
 const sorterNumbers = new Sorter(numbersCollection);
@@ -11,3 +12,13 @@ const charactersCollection = new CharactersCollection('rhwiYrNc');
 const sorterCharacters = new Sorter(charactersCollection);
 sorterCharacters.sort();
 console.log(charactersCollection.data);
+
+const linkList = new LinkedList();
+linkList.add(500);
+linkList.add(-10);
+linkList.add(-3);
+linkList.add(4);
+
+const listSorter = new Sorter(linkList);
+listSorter.sort();
+linkList.print();
